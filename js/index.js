1c07c74737a35303137885595848c32ada07e4da
KimKarina <kiiim_kk@mail.ru>
2022-03-15 03:50:53 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true, 
+  navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+  items:1
+})
