aeed982e9a5a9f17fd826005878fe1c9bee0b84b
KimKarina <kiiim_kk@mail.ru>
2022-03-16 03:35:11 +0600

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,62 @@
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true, 
-  navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
-  items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+    items: 1
 })
+
+
+$(document).ready(function() {
+    $('.chill1').click(function() {
+        $('.chill1').addClass('active');
+        $('.chill2').removeClass('active');
+        $('.chill3').removeClass('active');
+        $('.chill4').removeClass('active');
+        $('.about__landscape_middle-title').text("Chill Adventure");
+        $('.about__landscape_left-numberPage').text("01");
+        $('.about__landscape_left-pageInfo').html("Wonderful <br> adventures");
+    });
+});
+
+$(document).ready(function() {
+    $('.chill2').click(function() {
+        $('.chill1').removeClass('active');
+        $('.chill2').addClass('active');
+        $('.chill3').removeClass('active');
+        $('.chill4').removeClass('active');
+        $('.about__landscape_middle-title').text("Spooky Times");
+        $('.about__landscape_left-numberPage').text("02");
+        //Поменяй здесь в конце текст
+        $('.about__landscape_left-pageInfo').html("ПОМЕНЯЙ ТЕКСТ <br> ТЕКСТ");
+
+    });
+});
+
+$(document).ready(function() {
+    $('.chill3').click(function() {
+        $('.chill1').removeClass('active');
+        $('.chill2').removeClass('active');
+        $('.chill3').addClass('active');
+        $('.chill4').removeClass('active');
+        $('.about__landscape_middle-title').text("Desert Madness");
+        $('.about__landscape_left-numberPage').text("03");
+        //Поменяй здесь в конце текст
+        $('.about__landscape_left-pageInfo').html("ПОМЕНЯЙ ТЕКСТ <br> ТЕКСТ");
+
+
+    });
+});
+
+$(document).ready(function() {
+    $('.chill4').click(function() {
+        $('.chill1').removeClass('active');
+        $('.chill2').removeClass('active');
+        $('.chill3').removeClass('active');
+        $('.chill4').addClass('active');
+        $('.about__landscape_middle-title').text("Out in the wild");
+        $('.about__landscape_left-numberPage').text("04");
+        //Поменяй здесь в конце текст
+        $('.about__landscape_left-pageInfo').html("ПОМЕНЯЙ ТЕКСТ <br> ТЕКСТ");
+    });
+});
