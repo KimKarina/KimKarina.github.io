4eab2b8141d0d3d287e88f0a5dd607dccb4e8418
KimKarina <kiiim_kk@mail.ru>
2022-03-16 18:06:06 +0600

    js del

--- js/index.js
+++ js/index.js
@@ -19,44 +19,44 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
-    $('.chill2').click(function() {
-        $('.chill1').removeClass('active');
-        $('.chill2').addClass('active');
-        $('.chill3').removeClass('active');
-        $('.chill4').removeClass('active');
-        $('.about__landscape_middle-title').text("Spooky Times");
-        $('.about__landscape_left-numberPage').text("02");
-        //Поменяй здесь в конце текст
-        $('.about__landscape_left-pageInfo').html("SCARY AS HELL");
+// $(document).ready(function() {
+//     $('.chill2').click(function() {
+//         $('.chill1').removeClass('active');
+//         $('.chill2').addClass('active');
+//         $('.chill3').removeClass('active');
+//         $('.chill4').removeClass('active');
+//         $('.about__landscape_middle-title').text("Spooky Times");
+//         $('.about__landscape_left-numberPage').text("02");
+//         //Поменяй здесь в конце текст
+//         $('.about__landscape_left-pageInfo').html("SCARY AS HELL");
 
-    });
-});
+//     });
+// });
 
-$(document).ready(function() {
-    $('.chill3').click(function() {
-        $('.chill1').removeClass('active');
-        $('.chill2').removeClass('active');
-        $('.chill3').addClass('active');
-        $('.chill4').removeClass('active');
-        $('.about__landscape_middle-title').text("Desert Madness");
-        $('.about__landscape_left-numberPage').text("03");
-        //Поменяй здесь в конце текст
-        $('.about__landscape_left-pageInfo').html("GRAND DUNES <br> LANDSCAPE");
+// $(document).ready(function() {
+//     $('.chill3').click(function() {
+//         $('.chill1').removeClass('active');
+//         $('.chill2').removeClass('active');
+//         $('.chill3').addClass('active');
+//         $('.chill4').removeClass('active');
+//         $('.about__landscape_middle-title').text("Desert Madness");
+//         $('.about__landscape_left-numberPage').text("03");
+//         //Поменяй здесь в конце текст
+//         $('.about__landscape_left-pageInfo').html("GRAND DUNES <br> LANDSCAPE");
 
 
-    });
-});
+//     });
+// });
 
-$(document).ready(function() {
-    $('.chill4').click(function() {
-        $('.chill1').removeClass('active');
-        $('.chill2').removeClass('active');
-        $('.chill3').removeClass('active');
-        $('.chill4').addClass('active');
-        $('.about__landscape_middle-title').text("Out in the wild");
-        $('.about__landscape_left-numberPage').text("04");
-        //Поменяй здесь в конце текст
-        $('.about__landscape_left-pageInfo').html("BEAUTY OF <br> WILD NATURE");
-    });
-});
+// $(document).ready(function() {
+//     $('.chill4').click(function() {
+//         $('.chill1').removeClass('active');
+//         $('.chill2').removeClass('active');
+//         $('.chill3').removeClass('active');
+//         $('.chill4').addClass('active');
+//         $('.about__landscape_middle-title').text("Out in the wild");
+//         $('.about__landscape_left-numberPage').text("04");
+//         //Поменяй здесь в конце текст
+//         $('.about__landscape_left-pageInfo').html("BEAUTY OF <br> WILD NATURE");
+//     });
+// });
